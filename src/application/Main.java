c84473065784d70cc64428abab1302998e18e736
JoaoVitorNeves <jvitornevessilva@gmail.com>
2025-03-20 15:06:37 -0300

    Add initial project structure with shape hierarchy
    The commit initializes the project with a basic structure, including Java files for an abstract Shape class, concrete Circle and Rectangle classes, and a Color enum. It also incorporates IDE-specific configuration and ignore rules for IntelliJ, Eclipse, NetBeans, and VS Code. A Main class is included to set up the application entry point.

---
+++ src/application/Main.java
@@ -0,0 +1,15 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hola mundo");
+
+    }
+}
