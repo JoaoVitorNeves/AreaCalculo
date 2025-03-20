5bb8709e4a3d29a50456488b2f745b57e5708595
JoaoVitorNeves <jvitornevessilva@gmail.com>
2025-03-20 16:26:27 -0300

    Creating the main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import entitis.enums.Color;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -9,7 +11,20 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        System.out.println("Hola mundo");
+        
+        System.out.println("Enter the number of shapes: ");
+        int numOfShape = sc.nextInt();
+        
+        for (int i = 1; i<=numOfShape; i++){
+            System.out.println("Shape #" + i + "data: " );
+            System.out.print("Rectangle or circle: ");
+            char shape = sc.next().charAt(0);
+            System.out.print("Color (BLACK/BLUE/RED): ");
+            Color color = Color.valueOf(sc.next());
+
+
+
+        }
 
     }
 }
