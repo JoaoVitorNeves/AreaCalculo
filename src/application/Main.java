68b5b96faf1b2e6336c64e317f8efbf914e67e00
JoaoVitorNeves <jvitornevessilva@gmail.com>
2025-03-27 15:37:11 -0300

    finish the main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,13 @@
 package application;
 
+
+import entitis.Circle;
+import entitis.Rectangle;
+import entitis.Shape;
 import entitis.enums.Color;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -11,20 +17,37 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        
+        List<Shape> list = new ArrayList<>();
         System.out.println("Enter the number of shapes: ");
         int numOfShape = sc.nextInt();
         
         for (int i = 1; i<=numOfShape; i++){
-            System.out.println("Shape #" + i + "data: " );
+            System.out.println("Shape # " + i + " Data: " );
             System.out.print("Rectangle or circle: ");
             char shape = sc.next().charAt(0);
             System.out.print("Color (BLACK/BLUE/RED): ");
             Color color = Color.valueOf(sc.next());
+            if  (shape == 'r'){
+                System.out.print("Width: ");
+                Double width = sc.nextDouble();
+                System.out.print("Height: ");
+                Double height = sc.nextDouble();
+                list.add(new Rectangle(color, width, height));
+            }else{
+                System.out.print("Radius: ");
+                Double radius = sc.nextDouble();
+                list.add(new Circle(color, radius));
+
+            }
 
 
 
         }
-
+        sc.close();
+        System.out.println();
+        System.out.println("SHAPE AREA");
+        for (Shape shape : list){
+            System.out.println(shape.area());
+        }
     }
 }
